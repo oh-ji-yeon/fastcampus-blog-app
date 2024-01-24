6408c42cf4796bd1d1a7c99e2f97ba0665bd4320
oh-ji-yeon <o.delayed0106@gmail.com>
2024-01-24 17:23:53 +0900

    20240124

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import AuthContext from "context/AuthContext";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 interface PostListProps {
@@ -7,8 +10,39 @@ interface PostListProps {
 
 type TabType = "all" | "my";
 
+export interface PostProps {
+    id?: string;
+    title: string;
+    email: string;
+    summary: string;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    uid: string;
+}
+
 export default function PostList({ hasNavigation = true}: PostListProps) {
     const [activeTab, setActiveTab] = useState<TabType>("all");
+    const [posts, setPosts] = useState<PostProps[]>([]);
+    const { user } = useContext(AuthContext);
+
+    const getPosts = async() => {
+        const datas = await getDocs(collection(db, "posts"));
+
+        // console.log(datas);
+
+        datas?.forEach((doc) => {
+            // console.log(doc.data(), doc.id);
+            const dataObj = { ...doc.data(), id: doc.id };
+            setPosts((prev) => [...prev, dataObj as PostProps]);
+        });
+    };
+
+    // console.log(posts);
+
+    useEffect(() => {
+        getPosts();
+    }, []);
 
     return (
         <>
@@ -27,29 +61,28 @@ export default function PostList({ hasNavigation = true}: PostListProps) {
             </div>
         )}
         <div className="post__list">
-            {[...Array(10)].map((e, index) => (
-                <div key={index} className="post__box">
-                    <Link to={`/posts/${index}`}>
+            {posts?.length > 0 ? posts?.map((post, index) => (
+                <div key={post?.id} className="post__box">
+                    <Link to={`/posts/${post?.id}`}>
                         <div className="post__profile-box">
                             <div className="post__profile" />
-                            <div className="post__author-name">패스트캠퍼스</div>
-                            <div className="post__date">2024.01.12 금요일</div>
+                            <div className="post__author-name">{post?.email}</div>
+                            <div className="post__date">{post?.createdAt}</div>
                         </div>
-                        <div className="post__title">게시글 {index}</div>
-                        <div className="post__text">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Sequi tempora accusamus aliquam, hic magnam accusantium repudiandae fugiat qui veritatis 
-                            libero officia voluptatibus rem, reiciendis, architecto cupiditate cumque at mollitia unde!
-                        </div>
+                        <div className="post__title">{post?.title}</div>
+                        <div className="post__text">{post?.summary}</div>
+                    </Link>
+                    {post?.email === user?.email && (
                         <div className="post__utils-box">
                             <div className="post__delete">삭제</div>
-                            <div className="post__edit">수정</div>
+                            <div className="post__edit">
+                                <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+                            </div>
                         </div>
-                    </Link>
+                    )}
                 </div>
-            ))}
+            )) : (<div className="post__no-post">게시글이 없습니다.</div>)}
         </div>
-        </>
-        
+        </>  
     );
 }
--- src/components/postDetail.tsx
+++ src/components/postDetail.tsx
@@ -1,30 +1,55 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { PostProps } from "./PostList";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import Loader from "./Loader";
 
 export default function PostDetail() {
+    const [post, setPost] = useState<PostProps | null>(null);
+    const params = useParams();
+    // console.log(params?.id);
+
+    const getPost = async(id: string) => {
+        if(id) {
+            const docRef = doc(db, "posts", id);
+            const docSnap = await getDoc(docRef);
+            // console.log(docSnap?.data());
+
+            setPost({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+        }
+    };
+
+    const handleDelete = () => {
+        console.log("delete!");
+    };
+
+    // console.log(post);
+
+    useEffect(() => {
+        if(params?.id) getPost(params?.id);
+    }, [params?.id]);
+
     return (
         <>
         <div className="post__detail">
-            <div className="post__box">
-                <div className="post__title">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </div>
+            {post ? (
+                <div className="post__box">
+                <div className="post__title">{post?.title}</div>
                 <div className="post__profile-box">
                     <div className="post__profile" />
-                    <div className="post__author-name">패스트캠퍼스</div>
-                    <div className="post__date">2024.01.12 금요일</div>
+                    <div className="post__author-name">{post?.email}</div>
+                    <div className="post__date">{post?.createdAt}</div>
                     </div>
                     <div className="post__utils-box">
-                        <div className="post__delete">삭제</div>
+                        <div className="post__delete" role="presentation" onClick={handleDelete}>삭제</div>
                         <div className="post__edit">
-                            <Link to={`/posts/edit/1`}>수정</Link>
+                            <Link to={`/posts/edit/${post?.id}`}>수정</Link>
                         </div>
                     </div>
-                    <div className="post__text">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                        Sequi tempora accusamus aliquam, hic magnam accusantium repudiandae fugiat qui veritatis 
-                        libero officia voluptatibus rem, reiciendis, architecto cupiditate cumque at mollitia unde!
-                    </div>
-            </div>
+                    <div className="post__text post__text--pre-wrap">{post?.content}</div>
+                </div>
+            ) : <Loader />}
         </div>
         </>
     );
